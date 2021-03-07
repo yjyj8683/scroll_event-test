b8a6c5fd642afed9f32e9a3696b4fcdf9872fdb3
soyeon <ksy@optatumplatform.com>
2021-03-07 19:43:51 +0900

    스크롤 이벤트 js 추가

---
+++ main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  var $area2 = $('.area2');
+  var $area3 = $('.area3');
+  var $offset = 500;
+  var $area2OST = $area2.offset().top - $offset;
+  var $area3OST = $area3.offset().top - $offset;
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > $area2OST) {
+      $area2.find('.div2').addClass('animate');
+    }
+    if($(this).scrollTop() > $area3OST) {
+      $area3.find('.div3').addClass('animate');
+    }
+  });
+});
